2ed2e1a9aa26de9b354b5ab813ed1f4ebf0023ab
bfran <bfran@BRUNO>
2023-10-25 21:40:40 -0300

    Adicionar arquivos de projeto.

---
+++ SiteFF/Controllers/FuncionarioController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteFF.Models;
+using System.Diagnostics;
+
+namespace SiteFF.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SiteFF/Models/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿namespace SiteFF.Models
+{
+    public class Funcionario
+    {
+        public int Id1 { get; set; }
+        public string Nome { get; set; }
+        public string DataCon { get; set; }
+        public string DataNasc { get; set; }
+        public string Tel { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public string CTPS { get; set; }
+        public string PIS { get; set; }
+        public string Endereco { get; set; }
+        public double Salario { get; set; }
+        public int Cargo { get; set; }
+        public int Situacao { get; set; }
+        public int Empresa { get; set; }
+        public int Departamento { get; set; }
+        public int Contrato { get; set; }
+
+    }
+}
